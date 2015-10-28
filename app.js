176fd4a92072a74c382c38fcd0824cbc3f4b2f40
MarkWaldron <markewaldron@gmail.com>
2015-10-28 11:07:57 -0400

    Added setInterval to post current Date every 1000ms

--- app.js
+++ app.js
@@ -8,7 +8,10 @@
 var http = require('http');
 http.createServer(function (request, response){
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end();
+  setInterval(function(){
+    response.write(new Date() + "\n");
+  }, 1000);
+  //response.end();
 }).listen(3000, '127.0.0.1');
 console.log('Server is running at http://127.0.0.1:3000/');
 
